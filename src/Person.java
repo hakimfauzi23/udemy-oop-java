fa14688f89e819fc63a6d6b2e2bdb6d31e43db21
Hanif <hanif.hakim@spesolution.com>
2022-12-27 20:47:24 +0700

    Create Person Properties and Method!

--- src/Person.java
+++ src/Person.java
@@ -5,4 +5,19 @@ class Person {
      * Class Name And File Name Must Be Same!
      */
 
+    /*
+     * These below are Class field or properties!
+     * All object from this class will have them.
+     */
+    String name;
+    String address;
+    final String country = "Indonesia";
+
+    /*
+     * This is Class Method. can be called with
+     * init object first inside a variable.
+     */
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + ", My Name is " + name);
+    }
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -9,5 +9,22 @@ public class PersonApp {
         var person1 = new Person();
         Person person2 = new Person();
 
+        /*
+         * The class field can be modified each object separate
+         */
+        person1.name = "John Doe";
+        person1.address = "Venice";
+        
+        person2.name = "Will Smith";
+        person2.address = "Hollywood";
+        
+        System.out.println(person1.name);
+        System.out.println(person2.name);
+
+        /*
+         * This is how class method are called!
+         */
+        person1.sayHello("Johnny");
+        person2.sayHello("Sean");
     }
 }
