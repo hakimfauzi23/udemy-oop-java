7edc3409fbd604e7a692aff530ea63da83167b64
Hanif <hanif.hakim@spesolution.com>
2022-12-27 21:06:22 +0700

    'This' keyword to solve Variable Shadowing

--- src/Person.java
+++ src/Person.java
@@ -19,13 +19,13 @@ class Person {
      * Constructor is to set properties right after the object class is created!
      * Able to Overloading (create same method but with diff properties or param)
      */
-    Person(String paramName, String paramAddress) {
-        name = paramName;
-        address = paramAddress;
+    Person(String name, String address) {
+        this.name = name;
+        this.address = address;
     }
 
-    Person(String paramName) {
-        this(paramName, null);
+    Person(String name) {
+        this(name, null);
     }
 
     Person() {
@@ -36,7 +36,7 @@ class Person {
      * This is Class Method. can be called with
      * init object first inside a variable.
      */
-    void sayHello(String paramName) {
-        System.out.println("Hello " + paramName + ", My Name is " + name);
+    void sayHello(String name) {
+        System.out.println("Hello " + name + ", My Name is " + this.name);
     }
 }
