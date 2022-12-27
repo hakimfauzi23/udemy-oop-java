19568a8f31141d97ee84c838fac6a8f8ba824fef
Hanif <hanif.hakim@spesolution.com>
2022-12-27 21:16:08 +0700

    Method overriding in the child class by create same method in child class.

--- src/VicePresident.java
+++ src/VicePresident.java
@@ -4,4 +4,8 @@ class VicePresident extends Manager {
      * All of properties and method Manager class can be used
      * by VicePresident Object
      */
+
+    void sayHello(String name) {
+        System.out.println("Hello " + name + ", My Name is VP" + this.name);
+    }
 }
