da15867518a1ad6cfba0345d623fdd78765f1a97
Hanif <hanif.hakim@spesolution.com>
2022-12-27 20:38:27 +0700

    Create Person Class and Person Object.

---
+++ src/Person.java
@@ -0,0 +1,8 @@
+class Person {
+
+    /*
+     * This Is A Person Class
+     * Class Name And File Name Must Be Same!
+     */
+
+}
---
+++ src/PersonApp.java
@@ -0,0 +1,13 @@
+public class PersonApp {
+    public static void main(String[] args) {
+
+        /*
+         * This is how to create object from Class Person
+         * Each Object is independent! not related from another object
+         * that has same class.
+         */
+        var person1 = new Person();
+        Person person2 = new Person();
+
+    }
+}
