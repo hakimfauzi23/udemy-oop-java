4fc1544bcb8b6e00994c6bc8a64119a484dd72fc
Hanif <hanif.hakim@spesolution.com>
2022-12-28 09:07:36 +0700

    Create Package in Java!

---
+++ src/hanif/hakim/dev/Product.java
@@ -0,0 +1,15 @@
+package hanif.hakim.dev;
+
+public class Product {
+    /*
+     * Create Product class inside hanif.hakim.dev package!
+     */
+
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
